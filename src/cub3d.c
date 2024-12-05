3eb9c9e8e3a2efe14740759c316abf6141015244
Dlouu <135530996+Dlouu@users.noreply.github.com>
2024-12-05 10:59:26 +0100

    Dlou : 05/12 soon letsgo

--- src/cub3d.c
+++ src/cub3d.c
@@ -3,14 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbaumgar <mbaumgar@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 15:14:31 by mbaumgar          #+#    #+#             */
-/*   Updated: 2024/10/22 20:53:41 by mbaumgar         ###   ########.fr       */
+/*   Updated: 2024/12/05 10:57:43 by mbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	init_cub(t_cub *cub)
+{
+	cub->no = NULL;
+	cub->so = NULL;
+	cub->ea = NULL;
+	cub->we = NULL;
+	cub->map = NULL;
+	cub->x = 0;
+	cub->y = 0;
+}
 
 int	main(void)
 {
@@ -18,9 +29,7 @@ int	main(void)
 
 	ft_printf("%sHello World%s\n", MAUVE, END);
 	cub.fd = open("map.cub", O_RDONLY);
-	cub.x = 0;
-	cub.y = 0;
-	cub.map = NULL;
+	init_cub(&cub);
 	if (cub.fd == -1)
 	{
 		ft_printf("%sError\n%s", MAUVE, END);
