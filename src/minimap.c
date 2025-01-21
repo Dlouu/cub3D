1915f5c6df169bd2450cc03e2d3ddd8d63847dcd
NilsLeVrai <nilslucas@wanadoo.fr>
2025-01-21 13:46:11 +0100

    Nils : minimap oui

---
+++ src/minimap.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minimap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 13:38:07 by niabraha          #+#    #+#             */
+/*   Updated: 2025/01/21 13:40:39 by niabraha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+
+void draw_tile(t_cub *cub, int x, int y, double cx, double cy, double rotation_angle, double radius_squared)
+{
+	int dx, dy;
+	double local_x, local_y, rotated_x, rotated_y;
+
+	dy = 0;
+	while (dy < 20)
+	{
+		dx = 0;
+		while (dx < 20)
+		{
+			local_x = x + dx - cx;
+			local_y = y + dy - cy;
+			rotate_pixel(local_x, local_y, cx, cy, rotation_angle, &rotated_x, &rotated_y);
+
+			if (rotated_x >= 0 && rotated_x < 600 && rotated_y >= 0 && rotated_y < 600)
+			{
+				int color = is_inside_circle(rotated_x, rotated_y, cx, cy, radius_squared) ? 0x00FF00FF : 0x00000000;
+				mlx_put_pixel(cub->img, (int)rotated_x, (int)rotated_y, color);
+			}
+			dx++;
+		}
+		dy++;
+	}
+}
+
+void ft_inside_circle_minimap(t_cub *cub)
+{
+    int		map_x;
+	int		map_y;
+    double	pos_x;
+    double	pos_y;
+    double	radius_squared;
+
+    map_y = 0;
+	radius_squared = 300 * 300;
+	//ft_find_player_position(cub, &pos_x, &pos_y);
+	pos_x = 300;
+	pos_y = 300;
+    while (cub->map[map_y])
+    {
+        map_x = 0;
+        while (cub->map[map_y][map_x])
+        {
+            if (cub->map[map_y][map_x] == '1')
+                draw_tile(cub, map_x * 20, map_y * 20, pos_x, pos_y, cub->rotation_angle, radius_squared);
+            map_x++;
+        }
+        map_y++;
+    }
+}
+
+void ft_edge_circle_minimap(t_cub *cub)
+{
+	int	x;
+	int	y;
+	int	r;
+	int	x0;
+	int	y0;
+	int	i;
+
+	r = 300;
+	x0 = 300;
+	y0 = 300;
+	i = 0;
+	while (i < 360)
+	{
+		x = r * cos(i * PI / 180);
+		y = r * sin(i * PI / 180);
+		mlx_put_pixel(cub->img, x0 + x, y0 + y, 0x00FF00FF);
+		i++;
+	}
+}
+
+void ft_minimap(t_cub *cub)
+{
+	ft_edge_circle_minimap(cub);
+	ft_inside_circle_minimap(cub);
+}
--- src/start_game.c
+++ src/start_game.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 13:50:41 by niabraha          #+#    #+#             */
-/*   Updated: 2025/01/13 18:51:19 by niabraha         ###   ########.fr       */
+/*   Updated: 2025/01/21 13:41:15 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,90 +19,51 @@
  0x00FFFF00 = jaune
 */
 
-static void	ft_inside_circle_minimap(t_cub *cub)
+void rotate_pixel(double local_x, double local_y, double cx, double cy, double rotation_angle, double *rotated_x, double *rotated_y)
 {
-	int		x, y;
-	double	rotated_x, rotated_y;
-	double	cx, cy, local_x, local_y;
-	int		dx, dy;
-	int		map_x, map_y;
+	*rotated_x = local_x * cos(rotation_angle) - local_y * sin(rotation_angle) + cx;
+	*rotated_y = local_x * sin(rotation_angle) + local_y * cos(rotation_angle) + cy;
+}
 
-	cx = 300; // Center of the minimap
-	cy = 300;
 
-	map_y = 0;
-	while (cub->map[map_y]) // Ensure cub->map[map_y] is not NULL
+int is_inside_circle(double rotated_x, double rotated_y, double cx, double cy, double radius_squared)
+{
+		double dist_squared; 
+		
+		dist_squared = (rotated_x - cx) * (rotated_x - cx) + (rotated_y - cy) * (rotated_y - cy);
+		return (dist_squared <= radius_squared);
+}
+
+//en haut bien
+
+static void ft_clear(t_cub *cub)
+{
+	int x;
+	int	y;
+
+	y = 0;
+	while (y < HEIGHT)
 	{
-		map_x = 0;
-		while (cub->map[map_y][map_x]) // Ensure cub->map[map_y][map_x] is not out of bounds
+		x = 0;
+		while (x < WIDTH)
 		{
-			if (cub->map[map_y][map_x] == '1')
-			{
-				x = map_x * 20;
-				y = map_y * 20;
-
-				dy = 0;
-				while (dy < 20)
-				{
-					dx = 0;
-					while (dx < 20)
-					{
-						local_x = x + dx - cx;
-						local_y = y + dy - cy;
-						rotated_x = local_x * cos(cub->rotation_angle) - local_y * sin(cub->rotation_angle) + cx;
-						rotated_y = local_x * sin(cub->rotation_angle) + local_y * cos(cub->rotation_angle) + cy;
-						if (rotated_x >= 0 && rotated_x < 600 && rotated_y >= 0 && rotated_y < 600)
-							mlx_put_pixel(cub->img, (int)rotated_x, (int)rotated_y, 0x00FF00FF);
-						dx++;
-					}
-					dy++;
-				}
-			}
-			map_x++;
+			mlx_put_pixel(cub->img, x, y, 0x00000000);
+			x++;
 		}
-		map_y++;
+		y++;
 	}
 }
 
-
-static void ft_edge_circle_minimap(t_cub *cub)
-{
-    int x;
-    int y;
-    int r;
-    int x0;
-    int y0;
-    int i;
-
-    r = 300;
-    x0 = 300;
-    y0 = 300;
-    i = 0;
-    while (i < 360)
-    {
-        x = r * cos(i * PI / 180);
-        y = r * sin(i * PI / 180);
-        mlx_put_pixel(cub->img, x0 + x, y0 + y, 0x00FF00FF);
-        i++;
-    }
-}
-
-static void ft_minimap(t_cub *cub)
-{
-    ft_edge_circle_minimap(cub);
-    ft_inside_circle_minimap(cub);
-}
-
-
-static void ft_display(void *param)
+void ft_display(void *param)
 {
 	t_cub *cub;
 
 	cub = (t_cub*)param;
+	ft_clear(cub);
 	ft_minimap(cub);
 }
 
-static void ft_hook(void *param)
+void ft_hook(void *param)
 {
 	t_cub *cub;
 
@@ -113,10 +74,7 @@ static void ft_hook(void *param)
 		exit(0);
 	}
 	if (mlx_is_key_down(cub->mlx, MLX_KEY_RIGHT))
-	{
-		//ft-transparent
 		cub->rotation_angle += 0.05;
-	}
 	if (mlx_is_key_down(cub->mlx, MLX_KEY_LEFT))
 		cub->rotation_angle -= 0.05;
 	if (cub->rotation_angle > 2 * PI)
