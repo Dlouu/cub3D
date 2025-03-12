99d462ca0f245f3e831e7d260d13b075350c12c8
Dlouu <135530996+Dlouu@users.noreply.github.com>
2025-03-12 16:27:52 +0100

    Dlou : 12/03 oui je sais je ne suis pas maniaque

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: mbaumgar <mbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 15:35:18 by niabraha          #+#    #+#             */
-/*   Updated: 2025/03/10 11:18:27 by mbaumgar         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:24:40 by mbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,8 @@ static void	draw_textured_column(t_cub *cub, int i, \
 		else if (pixel_y <= cub->wall_bottom)
 		{
 			dist_to_top = pixel_y - cub->wall_top;
-			offset_y = ((double)dist_to_top / \
-				(cub->wall_bottom - cub->wall_top)) * texture->height;
+			offset_y = ((double)dist_to_top \
+				/ (cub->wall_bottom - cub->wall_top)) * texture->height;
 			color = custom_texture_color(texture, x, (int)offset_y);
 			mlx_put_pixel(cub->img, i, pixel_y, color);
 		}
@@ -69,8 +69,8 @@ void	draw_walls(t_cub *cub, int i, double wall_hit)
 		cast_ray(cub->tmp_angle, start_x, start_y, cub);
 		cub->ray_distance = sqrt(pow(cub->ray_x - start_x, 2) \
 			+ pow(cub->ray_y - start_y, 2));
-		cub->corrected_distance = cub->ray_distance * \
-			cos(cub->tmp_angle - cub->rotation_angle);
+		cub->corrected_distance = cub->ray_distance \
+			* cos(cub->tmp_angle - cub->rotation_angle);
 		cub->wall_height = (TILE / cub->corrected_distance) * PROJ_PLANE;
 		cub->wall_top = (HEIGHT / 2) - (cub->wall_height / 2);
 		cub->wall_bottom = (HEIGHT / 2) + (cub->wall_height / 2);
